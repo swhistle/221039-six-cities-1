08f5e5a1f4d87778de7a6e5af6ad3ee3089a5ab1
Alexander Svistunov <alexsvistunov73@gmail.com>
2019-05-30 22:54:41 +0300

    fix app.test

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -2,9 +2,34 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import {App} from './app.jsx';
 
+const TEST_OFFERS = [
+  {
+    id: 1,
+    cityId: 1,
+    type: `Private room`,
+    name: `Wood and stone place`,
+    photoSrc: `img/room.jpg`,
+    link: ``,
+    price: `80`,
+    rating: 4,
+    coordinates: [52.3909553943508, 4.85309666406198]
+  }
+];
+
+const CITY_ID = 1;
+
+const onGetOffers = jest.fn(() => {});
+
+const onChangeCity = jest.fn(() => {});
+
 it(`App correctly renders`, () => {
   const app = renderer
-    .create(<App/>)
+    .create(<App
+      offers={TEST_OFFERS}
+      cityId={CITY_ID}
+      onchangecity={onChangeCity}
+      onGetOffers={onGetOffers}
+    />)
     .toJSON();
 
   expect(app).toMatchSnapshot();
