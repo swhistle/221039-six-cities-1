b5649cbd17ad93ff9da6a5a85ae4a8e3b681f5a1
Alexander Svistunov <alexsvistunov73@gmail.com>
2019-06-08 11:25:44 +0300

    add authorization feature

--- src/reducer.js
+++ src/reducer.js
@@ -1,18 +1,33 @@
 const initialState = {
   city: null,
-  offers: []
+  offers: [],
+  isAuthorizationRequired: false,
+  user: null
 };
 
 export const Operations = {
   loadOffers: () => (dispatch, _getState, api) => {
     return api.get(`/hotels`)
       .then((response) => dispatch(ActionCreators[Actions.GetOffersList](response.data)));
+  },
+  signIn: (signInData) => (dispatch, _getState, api) => {
+    return api.post(`/login`, {
+      email: signInData.email,
+      password: signInData.password
+    })
+      .then((response) => {
+        if (response.status === 200) {
+          dispatch(ActionCreators[Actions.SignIn](response.data));
+        }
+      });
   }
 };
 
 const Actions = {
   ChangeCity: `CHANGE_CITY`,
-  GetOffersList: `GET_OFFERS_LIST`
+  GetOffersList: `GET_OFFERS_LIST`,
+  ChangeAuthorizationRequirement: `CHANGE_AUTHORIZATION_REQUIREMENT`,
+  SignIn: `SIGN_IN`,
 };
 
 const ActionCreators = {
@@ -32,6 +47,22 @@ const ActionCreators = {
       }
     };
   },
+  [Actions.ChangeAuthorizationRequirement]: (isAuthorizationRequired) => {
+    return {
+      type: Actions.ChangeAuthorizationRequirement,
+      payload: {
+        isAuthorizationRequired
+      }
+    };
+  },
+  [Actions.SignIn]: (signInData) => {
+    return {
+      type: Actions.SignIn,
+      payload: {
+        signInData
+      }
+    };
+  }
 };
 
 const reducer = (state = initialState, action) => {
@@ -45,6 +76,17 @@ const reducer = (state = initialState, action) => {
       return Object.assign({}, state, {
         offers: action.payload.offers,
         city: action.payload.offers[0].city
+      });
+
+    case Actions.ChangeAuthorizationRequirement:
+      return Object.assign({}, state, {
+        isAuthorizationRequired: action.payload.isAuthorizationRequired
+      });
+
+    case Actions.SignIn:
+      return Object.assign({}, state, {
+        user: action.payload.signInData,
+        isAuthorizationRequired: false
       });
 
     default:
